497a57b6f6188592cb3e4e7e353c0b360b96c714
Alekai McAdam <am8578@nyu.edu>
2019-10-09 00:27:29 -0400

    accidentally didn't upload everything last time bc i am an oaf

---
+++ dev midterm/Assets/Scripts/startGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
